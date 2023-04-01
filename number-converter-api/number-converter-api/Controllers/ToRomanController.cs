2cea6935491958d1bd0101605361d696588ba574
Jesper Jensen <jesper.j@maersk.com>
2023-04-01 16:27:59 +0200

    chore: changed API projcet to correct type

---
+++ number-converter-api/number-converter-api/Controllers/ToRomanController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace number_converter_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ToRomanController : Controller
+{
+    [HttpPost(Name = "ToRoman")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ToRoman()
+    {
+        throw new NotImplementedException();
+    }
+}
