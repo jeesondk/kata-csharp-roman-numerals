c8fe85a76c66faa04f738599e1fa703db3fd786c
Jesper Jensen <jesper.j@maersk.com>
2023-04-01 15:58:48 +0200

    feat: added boundary condition check

--- number-converter-api/number-converter/NumberConverter.cs
+++ number-converter-api/number-converter/NumberConverter.cs
@@ -2,8 +2,53 @@
 
 public class NumberConverter
 {
-    public object ToRoman(int i)
+    private readonly Dictionary<int, string> _arabicToRomanLookUp;
+
+    public NumberConverter()
     {
-        throw new NotImplementedException();
+        _arabicToRomanLookUp = InitArabicToRomanLookUp();
+    }
+
+    public string ToRoman(int i)
+    {
+        IsValid(i);
+        
+        return _arabicToRomanLookUp[i];
+    }
+    
+    
+    private Dictionary<int, string> InitArabicToRomanLookUp()
+    {
+        var collection = new Dictionary<int, string>
+        {
+            {1, "I"},
+            {2, "II"},
+            {3, "III"},
+            {4, "IV"},
+            {5, "V"},
+            {6, "VI"},
+            {7, "VII"},
+            {8, "VIII"},
+            {9, "IX"},
+            {10, "X"}
+        };
+
+        return collection;
+    }
+
+    private void IsValid(int i)
+    {
+        Tuple<bool, string> state;
+
+        state = i switch
+        {
+            < 0 => new Tuple<bool, string>(false, "Negative numbers are not allowed"),
+            0 => new Tuple<bool, string>(false, "Only positive numbers allowed"),
+            > 5000 => new Tuple<bool, string>(false, "Roman numerals does not go beyond 5000"),
+            _ => new Tuple<bool, string>(true, string.Empty)
+        };
+        
+        if(state.Item1 != true)
+            throw new ArgumentException(state.Item2);
     }
 }
