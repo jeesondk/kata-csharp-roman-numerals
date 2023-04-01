bd39835c47f713f7b696ef3aa67a5eefb62f3c29
Jesper Jensen <jesper.j@maersk.com>
2023-04-01 16:28:31 +0200

    test: Added controller init test

--- number-converter-api/number-converter-api-tests/ToRomanControllerTests.cs
+++ number-converter-api/number-converter-api-tests/ToRomanControllerTests.cs
@@ -1,9 +1,21 @@
+using FluentAssertions;
+using number_converter_api.Controllers;
+
 namespace number_converter_api_tests;
 
-public class UnitTest1
+public class ToRomanControllerTests
 {
-    [Fact]
-    public void Test1()
+    private ToRomanController _toRomanController;
+
+
+    public ToRomanControllerTests()
     {
+        _toRomanController = new ToRomanController();
+    }
+    
+    [Fact]
+    public void CanInitController()
+    {
+        _toRomanController.Should().NotBeNull().And.BeOfType<ToRomanController>();
     }
 }
